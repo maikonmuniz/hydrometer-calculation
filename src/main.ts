9037a5270bbf1b4887db95d46ff3c656eef601ca
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-09-15 16:23:38 -0300

    feat: add swegger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Documentação')
+    .setDescription('Teste para empresa StartSoft')
+    .setVersion('1.0')
+    .addTag('Routes')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

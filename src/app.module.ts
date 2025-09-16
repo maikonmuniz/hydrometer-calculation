d80ce8e2a95501e9c23f3d638589dcd13a1d2e54
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-09-16 11:34:33 -0300

    feat: add module condominium

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CondominiumModule } from './condominium/condominium.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    CondominiumModule,
   ],
 })
 export class AppModule {}

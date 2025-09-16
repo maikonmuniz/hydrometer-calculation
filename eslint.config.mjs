d2bc82ce12d2929598674b3503e5bd4e9ef5a435
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-09-16 11:38:18 -0300

    feat: config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,6 +35,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     },
   },
 );
